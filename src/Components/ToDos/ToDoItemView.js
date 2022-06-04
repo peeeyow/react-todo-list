29a1042318bd2127504c12f1d216889e776a12ae
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-04 16:11:28 +0800

    Add ToDoItemView component

--- src/Components/ToDos/ToDoItemView.js
+++ src/Components/ToDos/ToDoItemView.js
@@ -1,3 +1,11 @@
+import Button from "../UI/Button";
+
 export default function ToDoItemView(props) {
-  return <span>{props.done ? <s>{props.name}</s> : props.name}</span>;
+  return (
+    <>
+      <input type="checkbox" onChange={props.onTick} />
+      <span>{props.done ? <s>{props.name}</s> : props.name}</span>
+      <Button onClick={props.onClickEdit}>Edit</Button>
+    </>
+  );
 }
